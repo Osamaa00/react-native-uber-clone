9cf4c1e671e38f8be4e575a6b7e27d23851d1a3a
Osama <osamanadeem679@gmail.com>
2022-04-02 12:49:50 +0500

    added google places api

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, SafeAreaView, Image} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
 
 const HomeScreen = () => {
   return (
@@ -18,6 +20,11 @@ const HomeScreen = () => {
           }}
         />
       </View>
+      <GooglePlacesAutocomplete
+          placeholder='Where From?'
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+      />
       <NavOptions />
     </SafeAreaView>
   );
